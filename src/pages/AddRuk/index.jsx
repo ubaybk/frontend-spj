ef4f190696975c63d8e4abadb5b7c99951cf832d
Bayu Kurniawan <bayu05061992@gmail.com>
2024-12-05 23:05:55 +0700

    add input ruk

--- src/pages/AddRuk/index.jsx
+++ src/pages/AddRuk/index.jsx
@@ -6,11 +6,37 @@ import Header from "../../components/header";
 import { IoIosAddCircle } from "react-icons/io";
 import { Link } from "react-router-dom";
 
-
-
 const AddRuk = () => {
   const [tempatTugas, setTempatTugas] = useState("");
-  const [pjProgram, setPjProgram] = useState("");
+  const [pokja, setPokja] = useState("");
+  const [kegiatan, setKegiatan] = useState ("")
+  const [subKegiatan, setSubKegiatan] = useState("")
+
+  const optionsTugas = [
+    "Puskesmas Cilandak",
+    "PUSTU Pondok Labu",
+    "PUSTU Cilandak Barat",
+    "PUSTU Lebak Bulus",
+    "PUSTU Gandaria Selatan",
+    "PUSTU Cipete Selatan",
+  ];
+
+  const optionPokja = [
+    "Admen",
+    "UKP",
+    "UKM"
+  ]
+
+  const optionKegiatan = [
+    "Pengumpulan Data",
+    "Pengumpulan Rizal",
+    "Pengumpulan Anggy"
+  ]
+  const optionSubKegiatan = [
+    "Kegiatan 1",
+    "Kegiatan 2",
+    "Kegiatan 3"
+  ]
 
   const handleSubmit = async () => {
     const auth = getAuth();
@@ -25,7 +51,9 @@ const AddRuk = () => {
       // Tambahkan data ke subkoleksi "ruk_data"
       await addDoc(collection(db, "ruk_data"), {
         tempatTugas,
-        pjProgram,
+        pokja,
+        kegiatan,
+        subKegiatan,
         createdBy: user.email,
         createdAt: serverTimestamp(), // Waktu server
         userId: user.uid, // Untuk filter data berdasarkan pengguna
@@ -33,7 +61,9 @@ const AddRuk = () => {
 
       alert("Data berhasil ditambahkan!");
       setTempatTugas("");
-      setPjProgram("");
+      setPokja("");
+      setKegiatan("")
+      setSubKegiatan("")
     } catch (error) {
       console.error("Error adding document: ", error);
       alert("Gagal menambahkan data.");
@@ -45,29 +75,87 @@ const AddRuk = () => {
       <div className="flex">
         <Header />
         <div className="p-4">
-            <Link to={'/ruk'}>
-          <div className=" flex items-center cursor-pointer hover:underline">
-            <IoIosAddCircle className="text-[25px]" />
-            <h1>Tambah Data Perencanaan</h1>
-          </div>
-            </Link>
+          <Link to={"/ruk"}>
+            <div className=" flex items-center cursor-pointer hover:underline">
+              <IoIosAddCircle className="text-[25px]" />
+              <h1>Tambah Data Perencanaan</h1>
+            </div>
+          </Link>
+          <div className="grid grid-cols-3 gap-5 border p-2">
           <div>
             <h1>Tempat Tugas</h1>
-            <input
-              type="text"
-              placeholder="Masukkan tempat tugas"
+            <select
               value={tempatTugas}
               onChange={(e) => setTempatTugas(e.target.value)}
-            />
+              className="border rounded p-2 w-full"
+            >
+              <option value="" disabled>
+                Pilih tempat tugas
+              </option>
+              {optionsTugas.map((option, index) => (
+                <option key={index} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+            {/* {tempatTugas && (
+              <p className="mt-2">Tempat tugas yang dipilih: {tempatTugas}</p>
+            )} */}
           </div>
           <div>
-            <h1>PJ Program</h1>
-            <input
-              type="text"
-              placeholder="Masukkan PJ program"
-              value={pjProgram}
-              onChange={(e) => setPjProgram(e.target.value)}
-            />
+            <h1>Pokja</h1>
+            <select
+              value={pokja}
+              onChange={(e) => setPokja(e.target.value)}
+              className="border rounded p-2 w-full"
+            >
+              <option value="" disabled>
+                Pilih Pokja
+              </option>
+              {optionPokja.map((option, index) => (
+                <option key={index} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div>
+            <h1>Kegiatan</h1>
+            <select
+              value={kegiatan}
+              onChange={(e) => setKegiatan(e.target.value)}
+              className="border rounded p-2 w-full"
+            >
+              <option value="" disabled>
+                Pilih Kegiatan
+              </option>
+              {optionKegiatan.map((option, index) => (
+                <option key={index} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div>
+            <h1>Sub Kegiatan</h1>
+            <select
+              value={subKegiatan}
+              onChange={(e) => setSubKegiatan(e.target.value)}
+              className="border rounded p-2 w-full"
+            >
+              <option value="" disabled>
+                Pilih Sub Kegiatan
+              </option>
+              {optionSubKegiatan.map((option, index) => (
+                <option key={index} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+          </div>
+         
+          
+
           </div>
           <button
             onClick={handleSubmit}
--- src/pages/Ruk/index.jsx
+++ src/pages/Ruk/index.jsx
@@ -15,13 +15,26 @@ import Header from "../../components/header";
 import { IoIosAddCircle } from "react-icons/io";
 import { Link } from "react-router-dom";
 
-
 const Ruk = () => {
   const [userName, setUserName] = useState("");
   const [userEmail, setUserEmail] = useState("");
   const [rukData, setRukData] = useState([]);
   const [editingId, setEditingId] = useState(null);
-  const [editForm, setEditForm] = useState({ tempatTugas: "", pjProgram: "" });
+  const [editForm, setEditForm] = useState({
+    tempatTugas: "",
+    pokja: "",
+    kegiatan: "",
+    subKegiatan: "",
+    aktivitas: "",
+    tujuan: "",
+    sasaran: "",
+    targetSasaran: "",
+    pj: "",
+    kebutuhanSumberDaya: "",
+    mitraKerja: "",
+    waktuPelaksanaan: "",
+    komponen: "",
+  });
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -45,11 +58,13 @@ const Ruk = () => {
         // Ambil semua data RUK dari Firestore untuk pengguna ini
         try {
           let rukQuery;
-          if (email === "admin@gmail.com"){
-            rukQuery = collection(db,"ruk_data")
-          } else{
-            
-             rukQuery = query(collection(db, "ruk_data"), where("userId", "==", user.uid));
+          if (email === "admin@gmail.com") {
+            rukQuery = collection(db, "ruk_data");
+          } else {
+            rukQuery = query(
+              collection(db, "ruk_data"),
+              where("userId", "==", user.uid)
+            );
           }
           const rukSnapshot = await getDocs(rukQuery);
 
@@ -73,7 +88,21 @@ const Ruk = () => {
 
   const handleEdit = (id, currentData) => {
     setEditingId(id);
-    setEditForm({ tempatTugas: currentData.tempatTugas, pjProgram: currentData.pjProgram });
+    setEditForm({
+      tempatTugas: currentData.tempatTugas,
+      pokja: currentData.pokja,
+      kegiatan: currentData.kegiatan,
+      subKegiatan: currentData.subKegiatan,
+      aktivitas: currentData.aktivitas,
+      tujuan: currentData.tujuan,
+      sasaran: currentData.sasaran,
+      targetSasaran: currentData.targetSasaran,
+      pj: currentData.pj,
+      kebutuhanSumberDaya: currentData.kebutuhanSumberDaya,
+      mitraKerja: currentData.mitraKerja,
+      waktuPelaksanaan: currentData.waktuPelaksanaan,
+      komponen: currentData.komponen,
+    });
   };
 
   const handleUpdate = async (id) => {
@@ -84,7 +113,21 @@ const Ruk = () => {
         prev.map((item) => (item.id === id ? { ...item, ...editForm } : item))
       );
       setEditingId(null);
-      setEditForm({ tempatTugas: "", pjProgram: "" });
+      setEditForm({
+        tempatTugas: "",
+        pokja: "",
+        kegiatan: "",
+        subKegiatan: "",
+        aktivitas: "",
+        tujuan: "",
+        sasaran: "",
+        targetSasaran: "",
+        pj: "",
+        kebutuhanSumberDaya: "",
+        mitraKerja: "",
+        waktuPelaksanaan: "",
+        komponen: "",
+      });
     } catch (error) {
       console.error("Error updating RUK data: ", error);
     }
@@ -108,84 +151,286 @@ const Ruk = () => {
     <>
       <div className="flex">
         <Header />
-        <div className="p-4">
-          <h1 className="text-2xl font-bold">Dashboard</h1>
-         <Link to={'/addruk'}>
-          <div className=" flex items-center cursor-pointer hover:underline">
-            <IoIosAddCircle className="text-[25px]" />
-            <h1>Tambah Data Perencanaan</h1>
+        <div className="p-4 w-full">
+          <div className="flex items-center justify-between">
+            <div>
+              <h1 className="text-2xl font-bold">Dashboard</h1>
+              <Link to={"/addruk"}>
+                <div className=" flex items-center cursor-pointer hover:underline bg-blue-400 rounded-xl p-2 text-white">
+                  <IoIosAddCircle className="text-[25px]" />
+                  <h1>Tambah Data Perencanaan</h1>
+                </div>
+              </Link>
+            </div>
+            <div className="text-blue-500">
+              {userName ? (
+                <>
+                  <p>Selamat datang, {userName}!</p>
+                  <p>Email Anda: {userEmail}</p>
+                </>
+              ) : (
+                <p>Data pengguna tidak tersedia.</p>
+              )}
+            </div>
           </div>
-         </Link>
-          {userName ? (
-            <>
-              <p>Selamat datang, {userName}!</p>
-              <p>Email Anda: {userEmail}</p>
-            </>
-          ) : (
-            <p>Data pengguna tidak tersedia.</p>
-          )}
 
-          <h2 className="text-xl font-semibold mt-4">Data Perencanaan (RUK):</h2>
-          {rukData && rukData.length > 0 ? (
-            rukData.map((item) => (
-              <div key={item.id} className="bg-gray-100 p-4 rounded shadow mb-4">
-                {editingId === item.id ? (
-                  <div>
-                    <input
-                      type="text"
-                      value={editForm.tempatTugas}
-                      onChange={(e) => setEditForm({ ...editForm, tempatTugas: e.target.value })}
-                      placeholder="Edit Tempat Tugas"
-                      className="border p-2 rounded mb-2 w-full"
-                    />
-                    <input
-                      type="text"
-                      value={editForm.pjProgram}
-                      onChange={(e) => setEditForm({ ...editForm, pjProgram: e.target.value })}
-                      placeholder="Edit PJ Program"
-                      className="border p-2 rounded mb-2 w-full"
-                    />
-                    <button
-                      onClick={() => handleUpdate(item.id)}
-                      className="bg-blue-500 text-white px-4 py-2 rounded mr-2"
-                    >
-                      Update
-                    </button>
-                    <button
-                      onClick={() => setEditingId(null)}
-                      className="bg-gray-500 text-white px-4 py-2 rounded"
-                    >
-                      Cancel
-                    </button>
+          <div className="flex  gap-3">
+            <div className=" w-[30%]">
+              <h2 className="text-xl font-semibold mt-4">
+                Data Perencanaan (RUK):
+              </h2>
+              {rukData && rukData.length > 0 ? (
+                rukData.map((item) => (
+                  <div
+                    key={item.id}
+                    className="bg-gray-100 p-4 rounded shadow mb-4"
+                  >
+                    {editingId === item.id ? (
+                      <div>
+                        <input
+                          type="text"
+                          value={editForm.tempatTugas}
+                          onChange={(e) =>
+                            setEditForm({
+                              ...editForm,
+                              tempatTugas: e.target.value,
+                            })
+                          }
+                          placeholder="Edit Tempat Tugas"
+                          className="border p-2 rounded mb-2 w-full"
+                        />
+
+                        <input
+                          type="text"
+                          value={editForm.pokja}
+                          onChange={(e) =>
+                            setEditForm({ ...editForm, pokja: e.target.value })
+                          }
+                          placeholder="Edit pokja"
+                          className="border p-2 rounded mb-2 w-full"
+                        />
+                        <input
+                          type="text"
+                          value={editForm.kegiatan}
+                          onChange={(e) =>
+                            setEditForm({
+                              ...editForm,
+                              kegiatan: e.target.value,
+                            })
+                          }
+                          placeholder="Edit kegiatan"
+                          className="border p-2 rounded mb-2 w-full"
+                        />
+                        <input
+                          type="text"
+                          value={editForm.subKegiatan}
+                          onChange={(e) =>
+                            setEditForm({
+                              ...editForm,
+                              subKegiatan: e.target.value,
+                            })
+                          }
+                          placeholder="Edit subKegiatan"
+                          className="border p-2 rounded mb-2 w-full"
+                        />
+                        <input
+                          type="text"
+                          value={editForm.aktivitas}
+                          onChange={(e) =>
+                            setEditForm({
+                              ...editForm,
+                              aktivitas: e.target.value,
+                            })
+                          }
+                          placeholder="Edit aktivitas"
+                          className="border p-2 rounded mb-2 w-full"
+                        />
+                        <input
+                          type="text"
+                          value={editForm.tujuan}
+                          onChange={(e) =>
+                            setEditForm({ ...editForm, tujuan: e.target.value })
+                          }
+                          placeholder="Edit tujuan"
+                          className="border p-2 rounded mb-2 w-full"
+                        />
+                        <input
+                          type="text"
+                          value={editForm.sasaran}
+                          onChange={(e) =>
+                            setEditForm({
+                              ...editForm,
+                              sasaran: e.target.value,
+                            })
+                          }
+                          placeholder="Edit sasaran"
+                          className="border p-2 rounded mb-2 w-full"
+                        />
+                        <input
+                          type="text"
+                          value={editForm.targetSasaran}
+                          onChange={(e) =>
+                            setEditForm({
+                              ...editForm,
+                              targetSasaran: e.target.value,
+                            })
+                          }
+                          placeholder="Edit targetSasaran"
+                          className="border p-2 rounded mb-2 w-full"
+                        />
+                        <input
+                          type="text"
+                          value={editForm.pj}
+                          onChange={(e) =>
+                            setEditForm({ ...editForm, pj: e.target.value })
+                          }
+                          placeholder="Edit pj"
+                          className="border p-2 rounded mb-2 w-full"
+                        />
+                        <input
+                          type="text"
+                          value={editForm.kebutuhanSumberDaya}
+                          onChange={(e) =>
+                            setEditForm({
+                              ...editForm,
+                              kebutuhanSumberDaya: e.target.value,
+                            })
+                          }
+                          placeholder="Edit kebutuhanSumberDaya"
+                          className="border p-2 rounded mb-2 w-full"
+                        />
+                        <input
+                          type="text"
+                          value={editForm.mitraKerja}
+                          onChange={(e) =>
+                            setEditForm({
+                              ...editForm,
+                              mitraKerja: e.target.value,
+                            })
+                          }
+                          placeholder="Edit mitraKerja"
+                          className="border p-2 rounded mb-2 w-full"
+                        />
+                        <input
+                          type="text"
+                          value={editForm.waktuPelaksanaan}
+                          onChange={(e) =>
+                            setEditForm({
+                              ...editForm,
+                              waktuPelaksanaan: e.target.value,
+                            })
+                          }
+                          placeholder="Edit waktuPelaksanaan"
+                          className="border p-2 rounded mb-2 w-full"
+                        />
+                        <input
+                          type="text"
+                          value={editForm.komponen}
+                          onChange={(e) =>
+                            setEditForm({
+                              ...editForm,
+                              komponen: e.target.value,
+                            })
+                          }
+                          placeholder="Edit komponen"
+                          className="border p-2 rounded mb-2 w-full"
+                        />
+                        <button
+                          onClick={() => handleUpdate(item.id)}
+                          className="bg-blue-500 text-white px-4 py-2 rounded mr-2"
+                        >
+                          Update
+                        </button>
+                        <button
+                          onClick={() => setEditingId(null)}
+                          className="bg-gray-500 text-white px-4 py-2 rounded"
+                        >
+                          Cancel
+                        </button>
+                      </div>
+                    ) : (
+                      <div>
+                        <p>
+                          <strong>Tempat Tugas:</strong> {item.tempatTugas}
+                        </p>
+                        <p>
+                          <strong>Pokja:</strong> {item.pokja}
+                        </p>
+                        <p>
+                          <strong>Kegiatan:</strong> {item.kegiatan}
+                        </p>
+                        <p>
+                          <strong>Sub Kegiatan:</strong> {item.subKegiatan}
+                        </p>
+                        <p>
+                          <strong>Aktivitas:</strong> {item.aktivitas}
+                        </p>
+                        <p>
+                          <strong>Tujuan:</strong> {item.tujuan}
+                        </p>
+                        <p>
+                          <strong>Sasaran:</strong> {item.sasaran}
+                        </p>
+                        <p>
+                          <strong>Target Sasaran:</strong> {item.targetSasaran}
+                        </p>
+                        <p>
+                          <strong>PJ:</strong> {item.pj}
+                        </p>
+                        <p>
+                          <strong>Kebutuhan Sumber Daya:</strong>{" "}
+                          {item.kebutuhanSumberDaya}
+                        </p>
+                        <p>
+                          <strong>Mitra Kerja:</strong> {item.mitraKerja}
+                        </p>
+                        <p>
+                          <strong>Waktu Pelaksanaan:</strong>{" "}
+                          {item.waktuPelaksanaan}
+                        </p>
+                        <p>
+                          <strong>Komponen:</strong> {item.komponen}
+                        </p>
+                        <p>
+                          <strong>Dibuat Oleh:</strong> {item.createdBy}
+                        </p>
+                        <p>
+                          <strong>Dibuat Pada:</strong>{" "}
+                          {item.createdAt
+                            ? new Date(
+                                item.createdAt.seconds * 1000
+                              ).toLocaleString()
+                            : "N/A"}
+                        </p>
+                        <button
+                          onClick={() => handleEdit(item.id, item)}
+                          className="bg-yellow-500 text-white px-4 py-2 rounded mr-2"
+                        >
+                          Edit
+                        </button>
+                        <button
+                          onClick={() => handleDelete(item.id)}
+                          className="bg-red-500 text-white px-4 py-2 rounded"
+                        >
+                          Delete
+                        </button>
+                      </div>
+                    )}
                   </div>
-                ) : (
-                  <div>
-                    <p><strong>Tempat Tugas:</strong> {item.tempatTugas}</p>
-                    <p><strong>PJ Program:</strong> {item.pjProgram}</p>
-                    <p><strong>Dibuat Oleh:</strong> {item.createdBy}</p>
-                    <p>
-                      <strong>Dibuat Pada:</strong>{" "}
-                      {item.createdAt ? new Date(item.createdAt.seconds * 1000).toLocaleString() : "N/A"}
-                    </p>
-                    <button
-                      onClick={() => handleEdit(item.id, item)}
-                      className="bg-yellow-500 text-white px-4 py-2 rounded mr-2"
-                    >
-                      Edit
-                    </button>
-                    <button
-                      onClick={() => handleDelete(item.id)}
-                      className="bg-red-500 text-white px-4 py-2 rounded"
-                    >
-                      Delete
-                    </button>
-                  </div>
-                )}
-              </div>
-            ))
-          ) : (
-            <p>Data RUK tidak tersedia.</p>
-          )}
+                ))
+              ) : (
+                <p>Data RUK tidak tersedia.</p>
+              )}
+            </div>
+            <div className=" w-[30%]">
+              <h2 className="text-xl font-semibold mt-4">
+                Verifikator :
+              </h2>
+             
+            </div>
+
+            
+          </div>
         </div>
       </div>
     </>
