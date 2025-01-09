208fa6f0308c554e1d19a4ce7b39408043f6cd0a
Bayu Kurniawan <bayu05061992@gmail.com>
2025-01-09 15:08:14 +0700

    add feature view POA and delete

--- src/pages/Poa/DataPoa/index.jsx
+++ src/pages/Poa/DataPoa/index.jsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
 import Header from "../../../components/header";
 import { IoIosAddCircle } from "react-icons/io";
-import { getDocs, collection } from "firebase/firestore";
+import { getDocs, collection, deleteDoc, doc } from "firebase/firestore";
 import { useState, useEffect } from "react";
 import db from "../../../../firebaseConfig";
+import { IoMdCloseCircle } from "react-icons/io";
 
 const DataPoa = () => {
   const [dataPoa, setDataPoa] = useState([]);
@@ -20,10 +21,24 @@ const DataPoa = () => {
 
     fetchData();
   }, []);
+
+  const handleDelete = async (id) => {
+    try {
+      await deleteDoc(doc(db, "poa_data", id));
+      // Perbarui state untuk menghapus item yang sudah dihapus dari UI
+      setDataPoa(dataPoa.filter((item) => item.id !== id));
+      alert("Data berhasil dihapus!");
+    } catch (error) {
+      console.error("Error deleting document: ", error);
+      alert("Gagal menghapus data.");
+    }
+  };
+
   console.log("POA DATA", dataPoa);
+
   return (
     <>
-      <div className="flex ">
+      <div className="flex">
         <div className="h-screen">
           <Header />
         </div>
@@ -34,46 +49,54 @@ const DataPoa = () => {
               <h1>TAMBAHAN DATA TAHUNAN P O A </h1>
             </div>
           </Link>
-          <div className="">
-            <Link to={"/viewpoa"}>
-              {dataPoa.map((item) => (
-                <div className=" bg-green-300 p-2 hover:bg-green-100  font-bold rounded-xl">
-                  <div className="flex gap-2 items-center justify-center">
-                    <h1>Data POA TAHUN</h1>
-                    <h1>{item.tahun}</h1>
+          <div>
+            {dataPoa.map((item) => (
+              <div key={item.id} className="flex">
+                <Link to={"/viewpoa"}>
+                  <div className="bg-green-300 p-2 hover:bg-green-100 font-bold rounded-xl mb-5">
+                    <div className="flex gap-2 items-center justify-center">
+                      <h1>Data POA TAHUN</h1>
+                      <h1>{item.tahun}</h1>
+                    </div>
+                    <div className="flex gap-3">
+                      <div className="flex-col bg-yellow-300 hover:bg-green-100 p-1">
+                        <h1>BARANG DAN JASA</h1>
+                        <h1>
+                          {new Intl.NumberFormat("id-ID", {
+                            style: "currency",
+                            currency: "IDR",
+                          }).format(item.totalBarangJasa)}
+                        </h1>
+                      </div>
+                      <div className="flex-col bg-yellow-300 p-1">
+                        <h1>MODAL PERALATAN DAN MESIN</h1>
+                        <h1>
+                          {new Intl.NumberFormat("id-ID", {
+                            style: "currency",
+                            currency: "IDR",
+                          }).format(item.totalModalGedungBangunan)}
+                        </h1>
+                      </div>
+                      <div className="flex-col bg-yellow-300 p-1">
+                        <h1>MODAL GEDUNG DAN BANGUNAN</h1>
+                        <h1>
+                          {new Intl.NumberFormat("id-ID", {
+                            style: "currency",
+                            currency: "IDR",
+                          }).format(item.totalModalPeralatanMesin)}
+                        </h1>
+                      </div>
+                    </div>
                   </div>
-                  <div className="flex gap-3">
-                    <div className="flex-col bg-yellow-300 hover:bg-green-100 p-1">
-                      <h1>BARANG DAN JASA</h1>
-                      <h1>
-                        {new Intl.NumberFormat("id-ID", {
-                          style: "currency",
-                          currency: "IDR",
-                        }).format(item.totalBarangJasa)}
-                      </h1>
-                    </div>
-                    <div className="flex-col bg-yellow-300 p-1">
-                      <h1>MODAL PERALATAN DAN MESIN</h1>
-                      <h1>
-                        {new Intl.NumberFormat("id-ID", {
-                          style: "currency",
-                          currency: "IDR",
-                        }).format(item.totalModalGedungBangunan)}
-                      </h1>
-                    </div>
-                    <div className="flex-col bg-yellow-300 p-1">
-                      <h1>MODAL GEDUNG DAN BANGUNAN</h1>
-                      <h1>
-                        {new Intl.NumberFormat("id-ID", {
-                          style: "currency",
-                          currency: "IDR",
-                        }).format(item.totalModalPeralatanMesin)}
-                      </h1>
-                    </div>
-                  </div>
+                </Link>
+                <div
+                  className="text-[40px] text-red-600 cursor-pointer"
+                  onClick={() => handleDelete(item.id)}
+                >
+                  <IoMdCloseCircle />
                 </div>
-              ))}
-            </Link>
+              </div>
+            ))}
           </div>
         </div>
       </div>
